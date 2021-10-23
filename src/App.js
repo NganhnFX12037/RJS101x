9466599fe5a24db40264f302b312c44bbae17191
Nganhnfx12037 <hangocnganbg@gmail.com>
2021-10-23 22:36:20 +0900

    LAP3_1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
+//import logo from './logo.svg';
+//import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './Components/MenuComponent.js';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
+      <Menu/>
     </div>
   );
 }
