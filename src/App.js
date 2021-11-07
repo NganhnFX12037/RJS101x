bc0548049317e6efa17d7678d6e72d6974fa976a
hangocngan <hangocnganbg@gmail.com>
2021-11-07 22:50:55 +0900

    LAP05_2 React Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { DISHES } from './shared/dishes.js';
-import Menu from './components/MenuComponent.js';
-import './App.css';
 import Main from './components/MainComponent';
-import { COMMENTS } from './shared/comment.js';
-//import { render } from 'react-dom';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <Main/>
-      </div>
+      <BrowserRouter>
+        <div >
+          <Main/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
